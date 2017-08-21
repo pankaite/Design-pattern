35538b9fac18a6414f868e347ed0f0c17f37e36f
pankaite <kaite.pan@gmail.com>
2017-08-21 09:51:46 +0800

    Iterator Pattern
    迭代器模式

---
+++ IteratorPattern/Container.java
@@ -0,0 +1,5 @@
+package com.kate.IteratorPattern;
+
+public interface Container {
+	public Iterator getIterator();
+}
---
+++ IteratorPattern/Iterator.java
@@ -0,0 +1,7 @@
+package com.kate.IteratorPattern;
+
+public interface Iterator {
+	
+	public boolean hasNext();
+	public Object next();
+}
